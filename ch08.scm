04254459533df81feb21a8f3cd98a64ba2ffea06
awaissyed <awais.m.syed@gmail.com>
2015-06-30 09:12:37 +0500

    add question 8.2, 8.12.discuss 8.3

--- ch08.scm
+++ ch08.scm
@@ -278,14 +278,125 @@ c (accumulate sentence '(awais was here))
 
 
 
-|#    
+ 
    
+8.10 Write a predicate true–for–all? that takes two arguments, a predicate procedure a
+nd a sentence. It should return #t
+if the predicate argument returns true for every word in the sentence.   
    
+  
+
+
    
+(every last '(algebra purple spaghetti tomato gnu))
+
    
-   
-   
-   
-   
-   
+ (define (square x)
+   (* x x))
+'(a e i o u)
+> (every square (keep even? '(87 4 7 12 0 5)))
+'(16 144 0)
+> (every first '(and i love her))
+'(a i l h)
+> (keep vowel? (every first '(and i love her)))
+'(a i)
+
+> (keep vowel? (every first '(and i love her)))
+'(a i)
+> (accumulate word (keep vowel? (every first '(and i love her))))
+'ai
+> (accumulate se (keep vowel? (every first '(and i love her))))
+'(a i)
+> ((repeated square 0) 25)
+25
+ 
+
+(define (f a)
+  ((keep even? a)))
+
+When you teach a class, people will get distracted if you say "um" too many times. Write a count–ums that 
+counts the number of times "um" appears in a sentence:
+|#
+
+;(define (count sent)
+ ; (accumulate + (every always–one sent)))
+#|
+8.2 Fill in the blanks in the following Scheme interactions:
+> (__________ vowel? 'birthday)
+IA
+answer.
+keep vowel? 'birthday)
+'ia
+Page 120
+> (________ first '(golden slumbers))
+(G S)
+answer.
+(every first '(golden slumbers))
+'(g s)
+> (________ '(golden slumbers))
+GOLDEN
+answer
+(first '(golden slumber))
+'golden
+> (________ ________ '(little child))
+(E D)
+answer
+(every last '(little child))
+'(e d)
+> (________ ________ (________ ________ '(little child)))
+ED
+(accumulate word (every last '(little child)))
+'ed
+> (________ + '(2 3 4 5))
+(2 3 4 5)
+no known.
+> (________ + '(2 3 4 5))
+14
+(accumulate + '(2 3 4 5))
+14
+
+need to discuss 8.3
+|#
+#|
+8.12 [11.2] When you teach a class, people will get distracted if you say "um" too many times. Write a count–ums that counts the number of times "um" appears in a sentence:
+> (count–ums
+  '(today um we are going to um talk about functional um programming))
+3
+|#
+
+(define (always–one arg)
+  1)
+
+(define (count sent)
+  (accumulate + (every always–one sent)))
+
+(define (um? wd)
+  (member? wd '(um)))
+
+(define (count-ums sent)
+  (count  (keep um? sent)))
+#|
+anwer
+(count-ums '(i um waiting um ))
+2
+
+
+|#
+
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
 
